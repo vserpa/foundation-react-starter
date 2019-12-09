4407621e73abbbef84dbe5fb8592fd56f23bf939
Carlos Mestre <carlos.mestre@gamersclub.com.br>
2019-12-09 19:14:42 -0200

    create first component

--- huntweb/src/App.js
+++ huntweb/src/App.js
@@ -1,9 +1,12 @@
 import React from "react";
+import Header from "./components/Header";
+
+import "./style.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <Header />
     </div>
   );
 }
---
+++ huntweb/src/components/Header/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import "./style.css";
+
+// stateless
+const Header = () => <header id="main-header">System Running</header>;
+
+// with state
+// class Header extends Component {
+//     render() {
+//         return <h1>Hello</h1>
+//     }
+// }
+
+export default Header;
